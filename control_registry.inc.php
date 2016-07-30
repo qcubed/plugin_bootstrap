24285f9c8048ed965646221d6ad5c271e4d97408
spekary <spekary@gmail.com>
2016-07-30 10:39:19 -0700

    Adding a registry file to specify a bootstrap text box as a kind of text box to use in a connector.

---
+++ control_registry.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Called by the ModelConnector Designer to create a list of controls appropriate for the given database field type.
+ * The control will  be available in the list of controls that appear in ModelConnector desginer dialog
+ * in the ControlClass entry.
+ */
+
+$controls[QControlCategoryType::Text][] = 'QCubed\Plugin\Bootstrap\TextBox';
+
